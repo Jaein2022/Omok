9a59425fe35271064d30eca3bd1627910a43b187
Jaein <dlwodls10@naver.com>
2023-09-19 12:04:15 +0900

    0919 이재인: 프로젝트 초기화.

---
+++ Source/Omok/OmokGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OmokGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OMOK_API AOmokGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
